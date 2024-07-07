7df20115d54164e40ecf38ec5dd019970e4f4f61
argusherd <a7752876@gmail.com>
2024-07-07 19:13:33 +0800

    fix: pagination path after deletion

--- routes/middlewares/pagination.ts
+++ routes/middlewares/pagination.ts
@@ -11,12 +11,12 @@ const router = Router();
 
 router.use((req: Request, res, next) => {
   const page = isNaN(req.query.page as any) ? 1 : Number(req.query.page);
-  const path = req.path;
+  const reqPath = req.path;
 
   req.currentPage = page;
   res.locals.currentPage = page;
-  res.locals.paginator = (page: number) =>
-    `${path}?${stringify({ ...req.query, page })}`;
+  res.locals.paginator = (page: number, path?: string) =>
+    `${path ?? reqPath}?${stringify({ ...req.query, page })}`;
 
   next();
 });
