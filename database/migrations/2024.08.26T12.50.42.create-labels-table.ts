11ff7a12c10de8c7f0d3990b021daa1fa7918453
argusherd <a7752876@gmail.com>
2024-08-28 16:00:43 +0800

    refactor: unique category_id and text in the labels table

--- database/migrations/2024.08.26T12.50.42.create-labels-table.ts
+++ database/migrations/2024.08.26T12.50.42.create-labels-table.ts
@@ -2,29 +2,40 @@ import { DataType } from "sequelize-typescript";
 import { Migration } from "../migrator";
 
 export const up: Migration = async ({ context: queryInterface }) => {
-  await queryInterface.createTable("labels", {
-    id: {
-      type: DataType.UUID,
-      primaryKey: true,
+  await queryInterface.createTable(
+    "labels",
+    {
+      id: {
+        type: DataType.UUID,
+        primaryKey: true,
+      },
+      category_id: {
+        type: DataType.UUID,
+        allowNull: false,
+        references: { key: "id", model: "categories" },
+        onDelete: "CASCADE",
+        unique: "unique_labels_category_text",
+      },
+      text: {
+        type: DataType.STRING,
+        allowNull: false,
+        unique: "unique_labels_category_text",
+      },
+      created_at: {
+        type: DataType.DATE,
+      },
+      updated_at: {
+        type: DataType.DATE,
+      },
     },
-    category_id: {
-      type: DataType.UUID,
-      allowNull: false,
-      references: { key: "id", model: "categories" },
-      onDelete: "CASCADE",
+    {
+      uniqueKeys: {
+        unique_labels_category_text: {
+          fields: ["category_id", "text"],
+        },
+      },
     },
-    text: {
-      type: DataType.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataType.DATE,
-    },
-    updated_at: {
-      type: DataType.DATE,
-    },
-  });
+  );
 };
 
 export const down: Migration = async ({ context: queryInterface }) => {
