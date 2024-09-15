07bc1bcf27b453f678896e1e022b11449740861e
argusherd <a7752876@gmail.com>
2024-09-15 22:55:10 +0800

    feat: suppoert dark mode

--- renderer/ts/app.ts
+++ renderer/ts/app.ts
@@ -2,6 +2,7 @@ import htmx from "htmx.org";
 window.htmx = htmx;
 
 import morph from "@alpinejs/morph";
+import persist from "@alpinejs/persist";
 import sort from "@alpinejs/sort";
 import Alpine from "alpinejs";
 import dayjs from "dayjs";
@@ -17,6 +18,7 @@ window.dayjs = dayjs;
 window.Alpine = Alpine;
 Alpine.plugin(morph);
 Alpine.plugin(sort);
+Alpine.plugin(persist);
 Alpine.data("currentTime", currentTime);
 Alpine.start();
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.{pug,js,ts}", "./renderer/css/app.css"],
+  darkMode: "selector",
   plugins: [addDynamicIconSelectors()],
   theme: {
     extend: {},
