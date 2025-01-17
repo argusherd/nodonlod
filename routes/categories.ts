53db1b360374337ba95988fbcb4bb68b670a2873
argusherd <a7752876@gmail.com>
2025-01-17 16:36:43 +0800

    refactor: category create page

--- routes/categories.ts
+++ routes/categories.ts
@@ -3,8 +3,16 @@ import Category from "../database/models/category";
 
 const router = Router();
 
-router.get("/", async (_req, res) => {
-  res.render("categories/_list", { categories: await Category.findAll() });
+router.get("/create/_select", (_req, res) => {
+  res.render("categories/_select");
+});
+
+router.get("/create/_options", async (_req, res) => {
+  res.render("categories/_options", { categories: await Category.findAll() });
+});
+
+router.get("/create/_inputs", (_req, res) => {
+  res.render("categories/_inputs");
 });
 
 export default router;
