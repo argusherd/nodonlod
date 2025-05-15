51d0bd6839bb51e7bffe2b2b06f6b21ba6df1185
argusherd <a7752876@gmail.com>
2025-05-15 15:21:05 +0800

    fix: wrong basePath in chapter playlist add index route

--- routes/chapters/playlists.ts
+++ routes/chapters/playlists.ts
@@ -38,7 +38,7 @@ router.get("/add", async (req: ChapterRequest, res) => {
     "_list" in req.query ? "playlists/add/_list" : "playlists/add/index";
 
   res.set("HX-Trigger", "open-modal").render(template, {
-    basePath: `/chapters/${req.chapter.id}/playlists`,
+    basePath: `/chapters/${req.chapter.id}`,
     playlists,
     count,
   });
