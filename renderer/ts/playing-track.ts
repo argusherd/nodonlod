c663444dcff3b0a702f0041ffa0974acd868d024
argusherd <a7752876@gmail.com>
2025-08-06 18:15:13 +0800

    feat: medium increases play count by one after 20% been played, 30 seconds elapsed, or chapter ends

--- renderer/ts/playing-track.ts
+++ renderer/ts/playing-track.ts
@@ -1,5 +1,8 @@
 const playingTrack = () => ({
   currentTime: 0,
+  prevTime: 0,
+  elapsed: 0,
+  isTrackPlayed: false,
   duration: 0,
   percentage: 50,
   volume: 100,
@@ -11,17 +14,33 @@ const playingTrack = () => ({
       try {
         const payload = JSON.parse(message.data);
 
-        if ("currentTime" in payload) {
-          this.currentTime = payload.currentTime;
-          this.percentage = Math.min(
-            (this.currentTime / this.duration) * 100,
-            100,
-          );
-        } else if ("duration" in payload) this.duration = payload.duration;
-        else if ("volume" in payload) this.volume = payload.volume;
+        if ("currentTime" in payload) this.setCurrentTime(payload.currentTime);
+        else if ("duration" in payload) {
+          this.duration = payload.duration;
+          this.prevTime = 0;
+          this.elapsed = 0;
+          this.isTrackPlayed = false;
+        } else if ("volume" in payload) this.volume = payload.volume;
         else if ("mute" in payload) this.mute = payload.mute;
       } catch (e) {}
     };
+  },
+  setCurrentTime(currentTime: number) {
+    this.currentTime = currentTime;
+    this.percentage = Math.min((this.currentTime / this.duration) * 100, 100);
+
+    if (this.currentTime - this.prevTime >= 1) {
+      this.prevTime = this.currentTime;
+      this.elapsed++;
+
+      if (
+        !this.isTrackPlayed &&
+        (this.elapsed >= 30 || this.elapsed >= this.duration / 5)
+      ) {
+        this.$dispatch("track-played");
+        this.isTrackPlayed = true;
+      }
+    }
   },
 });
 
